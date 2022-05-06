c66f1ebf63f8e14af409f90f9a996660417692be
Timur Alimbekov <tim.aivarov@gmail.com>
2022-05-06 12:19:24 +0600

    Homework - Add 1 and 2 task

--- Skutorenko_Swift.playground/Pages/lesson 16 - Methods.xcplaygroundpage/Contents.swift
+++ Skutorenko_Swift.playground/Pages/lesson 16 - Methods.xcplaygroundpage/Contents.swift
@@ -41,7 +41,6 @@ p.moveBy(byX: 1, andY: 1)
 print(p)
 
 
-
 enum Color {
     
     // ранее мы записывали данные через проперти - можем также записать данные в методе
@@ -82,18 +81,111 @@ color
 color.invert()
 color.printColor()
 
-
 //Если функция возвращает тип Color, то можно использовать другие методы в типе вот так - color.invert().printColor()
 color.invert().printColor()
 
 Color.numberOfElements()
 
 
-
 //Homework ->
-//task1:
-/* 1. Создайте тип Комната. У комнаты есть размеры W на H. И создайте тип Персонаж. У него есть координата в комнате X и Y. Реализуйте функцию, которая красивенько текстом будет показывать положение персонажа в комнате
+/*
+ 1. Создайте тип Комната. У комнаты есть размеры W на H. И создайте тип Персонаж. У него есть координата в комнате X и Y. Реализуйте функцию, которая красивенько текстом будет показывать положение персонажа в комнате
+*/
+
+/*
 2. Персонажу добавьте метод идти, который принимает энумчик лево, право, верх, вниз
 Этот метод должен передвигать персонажа. Реализуйте правило что персонаж не должен покинуть пределы комнаты. Подвигайте персонажа и покажите это графически
+*/
+
+/*
+3.
 Создать тип Ящик. У ящика также есть координата в комнате X и Y. Ящик также не может покидать пределы комнаты и ящик также должен быть распечатан вместе с персонажем в функции печати.
 */
+
+
+struct Player {
+    
+    var positionX = 1 {
+        didSet {
+            if positionX > Room.height || positionX <= 0 {
+                positionX = oldValue
+            }
+        }
+    }
+    
+    var positionY = 1 {
+        didSet {
+            if positionY > Room.width || positionY <= 0 {
+                positionY = oldValue
+            }
+        }
+    }
+
+    mutating func moveTo(direction: Direction) -> Player {
+        switch direction {
+        case .Right:
+            self.positionY += 1
+            printPlayerPosition()
+            break
+        case .Left:
+            self.positionY -= 1
+            printPlayerPosition()
+            break
+        case .Down:
+            self.positionX += 1
+            printPlayerPosition()
+            break
+        case .Up:
+            self.positionX -= 1
+            printPlayerPosition()
+            break
+        }
+        return self
+    }
+    
+    func printPlayerPosition() {
+        for x in 1...Room.width {
+            var row = ""
+            for y in 1...Room.width {
+                switch (x, y) {
+                case (self.positionX, self.positionY):
+                    row += Objects.Player.rawValue
+                    break
+                default:
+                    row += Objects.EmptyField.rawValue
+                }
+            }
+            print(row)
+        }
+    }
+}
+
+enum Objects : String {
+    case Player = "👨‍🎓"
+    case EmptyField = "⬜"
+}
+
+enum Direction {
+    case Up
+    case Down
+    case Left
+    case Right
+}
+
+class Room {
+    static let width = 8
+    static let height = 8
+}
+
+
+var newPlayer = Player()
+newPlayer.moveTo(direction: .Down)
+
+
+
+
+
+
+
+
+
