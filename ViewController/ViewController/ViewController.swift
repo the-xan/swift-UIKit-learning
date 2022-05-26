ccb4d164b310206adb82e083247e3a3fe73b88ef
Timur Alimbekov <tim.aivarov@gmail.com>
2022-05-26 17:29:16 +0600

    Working with Alerts and simple buttons

--- ViewController/ViewController/ViewController.swift
+++ ViewController/ViewController/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //label надо ставить вверх
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func showButton(_ sender: Any) {
+        self.alert(title: "Welcome!", message: "Write your name:", style: .alert)
+        self.welcomeLabel.text = "Hi, "
+    }
+    
+    //Actions надо ставить вниз
+    
+    // 2 -> Метод создания простого алерта
+    func alert(title: String, message: String, style: UIAlertController.Style) {
+        //Это простой алерт
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        //Это действия алерта
+        let alertAction = UIAlertAction(title: "Ok!", style: .cancel) { _ in
+            let text = alertController.textFields?.first?.text
+            self.welcomeLabel.text! += text ?? "Username"
+            print(text ?? "nil")
+        }
+        
+        // Добавить текстовое поле ввода
+        alertController.addTextField(){ textField in
+            // Конфигурация текстового поля - не видимый ввод 
+            textField.isSecureTextEntry = true
+        }
+        // Добавить алерту его действие
+        alertController.addAction(alertAction)
+        //?
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
 }
 
